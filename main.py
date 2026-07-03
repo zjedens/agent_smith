2500dbdc142ff62c81e21bb858a8d9cff17c2a7a
zjedens <z.j.edens@gmail.com>
2026-07-03 17:06:28 -0500

    Added OpenAI and OpenRouter APIs to project, and set up a simple client to send a prompt and print the model's response.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from agent-smith!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+def main():
+    load_dotenv()
+    
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None: raise Exception("Error! Unable to find API key")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        }
+    ]
+    completion = client.chat.completions.create(model="openrouter/free", messages=messages)
+    print(completion.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
