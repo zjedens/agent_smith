044b8c1350365d5cb4257a32be9c18020712d9d3
zjedens <z.j.edens@gmail.com>
2026-08-24 18:00:53 -0500

    Ch2L3 - Modified get_files_info to return directory contents for valid directories. Added __pycache__ to .gitignore.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,16 +2,23 @@
 import os
 from pprint import pprint
 
-def get_files_info(working_directory: str, directory:str = ".") -> str:
+def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
         abs_path = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(abs_path, directory))
 
         valid_target = os.path.commonpath([target_dir, abs_path]) == abs_path
+        is_target_dir = os.path.isdir(target_dir)
 
         if not valid_target: return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
-        if not os.path.isdir(directory): return f"Error: \"{directory}\" is not a directory."
+        if not is_target_dir: return f"Error: \"{directory}\" is not a directory."
 
-        return f"Success: \"{directory}\" is within the working directory."
+        contents: list[str] = []
+        print(f"Printing contents of {target_dir}")
+        for item in os.listdir(target_dir):
+            full_path = os.path.join(target_dir, item)
+            contents.append(f"{item}: file_size={os.path.getsize(full_path)}, is_dir={os.path.isdir(full_path)}")
+
+        return "\n".join(contents)
     except Exception as e:
         pprint(f"Error: {e}")
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -2,8 +2,8 @@
 from functions.get_files_info import get_files_info
 
 if __name__ == "__main__":
-    print(get_files_info("calculdator", "."))
-    print(get_files_info("calculdator", "/bin"))
-    print(get_files_info("calculdator", "../"))
-    print(get_files_info("calculdator", "main.py"))
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
+    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "../"))
     
